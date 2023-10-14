8ed297dcf55370e26a3f80a0469491c0408dc46a
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2023-10-14 16:06:32 +0500

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,104 @@
+let toggle = document.querySelector(".toggle")
+let showlink = document.querySelector(".showlink")
+let body = document.querySelector(".body")
+
+toggle.addEventListener('click', function() {
+    toggle.classList.toggle('toggle-active')
+    showlink.classList.toggle('showlink-active')
+    body.classList.toggle('body-active')
+})
+
+
+function createAndAddUserCard(userData) {
+    const cardContainer = document.createElement("div");
+    cardContainer.className = "card-container";
+
+    const proLabel = document.createElement("span");
+    proLabel.className = "pro";
+    proLabel.textContent = "PRO";
+
+    const userImage = document.createElement("img");
+    userImage.className = "round";
+    userImage.src = userData.imageSrc;
+    userImage.alt = "user";
+
+    const userName = document.createElement("h3");
+    userName.textContent = userData.name;
+
+    const userProfession = document.createElement("p");
+    userProfession.textContent = userData.profession;
+
+    const skillsContainer = document.createElement("div");
+    skillsContainer.className = "skills";
+
+    const skillsTitle = document.createElement("h6");
+    skillsTitle.textContent = "Darajasi";
+
+    const skillsList = document.createElement("ul");
+    userData.skills.forEach(skill => {
+        const skillItem = document.createElement("li");
+        skillItem.textContent = skill;
+        skillsList.appendChild(skillItem);
+    });
+
+    skillsContainer.appendChild(skillsTitle);
+    skillsContainer.appendChild(skillsList);
+
+    cardContainer.appendChild(proLabel);
+    cardContainer.appendChild(userImage);
+    cardContainer.appendChild(userName);
+    cardContainer.appendChild(userProfession);
+    cardContainer.appendChild(skillsContainer);
+
+    document.getElementById("second-section-card").appendChild(cardContainer);
+}
+
+// Пример использования функции для создания и добавления новой карточки
+const newUser1 = {
+    name: "Alice Johnson",
+    profession: "Math teacher",
+    skills: ["Ielts band 7", "2 years experience"],
+    imageSrc: "https://randomuser.me/api/portraits/women/35.jpg",
+};
+
+const newUser2 = {
+    name: "Bob Smith",
+    profession: "English teacher",
+    skills: ["Ielts band 7", "cefr: c1"],
+    imageSrc: "https://randomuser.me/api/portraits/men/60.jpg",
+};
+
+const newUser3 = {
+    name: "Bob Smith",
+    profession: "English teacher",
+    skills: ["Ielts band 7", "cefr: c1"],
+    imageSrc: "https://randomuser.me/api/portraits/men/60.jpg",
+};
+
+const newUser4 = {
+    name: "Bob Smith",
+    profession: "English teacher",
+    skills: ["Ielts band 7", "cefr: c1"],
+    imageSrc: "https://randomuser.me/api/portraits/men/60.jpg",
+};
+
+const newUser5 = {
+    name: "Bob Smith",
+    profession: "English teacher",
+    skills: ["Ielts band 7", "cefr: c1"],
+    imageSrc: "https://randomuser.me/api/portraits/men/60.jpg",
+};
+
+const newUser6 = {
+    name: "Bob Smith",
+    profession: "English teacher",
+    skills: ["Ielts band 7", "cefr: c1"],
+    imageSrc: "https://randomuser.me/api/portraits/men/60.jpg",
+};
+
+createAndAddUserCard(newUser1);
+createAndAddUserCard(newUser2);
+createAndAddUserCard(newUser3);
+createAndAddUserCard(newUser4);
+createAndAddUserCard(newUser5);
+createAndAddUserCard(newUser6);
